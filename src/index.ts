1bdc38f5fd285d54ea9edb7c9d2f708b22415fd0
Greg Shaw <greg@vitaler.com>
2019-05-13 13:15:19 +1200

    Adds module complation from typescript.

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+
+import lodashGet from 'lodash.get'
+
+function safeGet() {
+    var args = Array.prototype.slice.call(arguments)
+    var obj = args[0];
+    var path = args.slice(1).join(".")
+    return lodashGet(obj, path);
+}
+
+export default safeGet
